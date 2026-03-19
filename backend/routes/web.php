006bf825a41ba783eb0a231f5dbe8c0c94dcda99
Prashann <prashannraj@hotmail.com>
2026-03-19 19:35:17 +0545

    Security: Replace Laravel welcome page with secure JSON response

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebCaseController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'secure',
+        'message' => 'Advocate Pro API is running.',
+        'version' => '1.0.0',
+        'developed_by' => 'Appan Technology Pvt. Ltd.'
+    ]);
 });
 
 Route::resource('cases', WebCaseController::class);
